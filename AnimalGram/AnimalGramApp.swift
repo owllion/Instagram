c2c51f0be606f92f1e9d07ff127014c0701988e7
owllion <defrag55345@gmail.com>
2023-02-20 13:59:36 +0800

    Initial Commit

---
+++ AnimalGram/AnimalGramApp.swift
@@ -0,0 +1,17 @@
+//
+//  AnimalGramApp.swift
+//  AnimalGram
+//
+//  Created by Zheng yu hsin on 2023/2/20.
+//
+
+import SwiftUI
+
+@main
+struct AnimalGramApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
