aeb0ecc60655557cf1cca9bd59d368a323460a1c
owllion <defrag55345@gmail.com>
2023-03-12 20:39:33 +0800

    select photo screen done

--- AnimalGram/Views/Screens/PostImageView.swift
+++ AnimalGram/Views/Screens/PostImageView.swift
@@ -16,7 +16,6 @@ struct PostImageView: View {
     @Environment(\.dismiss) var dismiss
     
     @State var captionText: String = ""
-    @State var selection:Int = 0
     @Binding var images: [UIImage]
     
     
@@ -35,58 +34,46 @@ struct PostImageView: View {
                 Spacer()
             }
             
-            ScrollView(.horizontal, showsIndicators: false) {
+            ScrollView(.vertical, showsIndicators: false) {
                 
-                HStack(spacing: 15) {
+                VStack(spacing: 15) {
                     if let images = images {
                         ForEach(images[1...].indices, id: \.self) { index in
-                                                        Image(uiImage: images[index])
-                                                            .resizable()
-                                                            .aspectRatio(contentMode: .fit)
-                                                            .frame(width: UIScreen.main.bounds.width - 45)
-                                                            .cornerRadius(20)
-                                                    }
+                            Image(uiImage: images[index])
+                                .resizable()
+                                .scaledToFill()
+                                .frame(width: UIScreen.main.bounds.width - 45,height: 250)
+                                
+                                .cornerRadius(20)
+                        }
                     }
-//                    if let images = images {
-//                        TabView(selection: $selection) {
-//                            ForEach(images[1...].indices, id: \.self) { index in
-//                                Image(uiImage: images[index])
-//                                    .resizable()
-//                                    .aspectRatio(contentMode: .fit)
-//                                    .frame(width: UIScreen.main.bounds.width - 45)
-//                                    .cornerRadius(20)
-//                            }
-//                        }.tabViewStyle(PageTabViewStyle())
-//
-//                    }
                 }
             }
-               
             
-            TextField(
-                "",
-                text: $captionText,
-                prompt:
-                    Text("Add your caption here...")
-                    .foregroundColor(.gray)
-            )
-            .customTextField(background: Color.MyTheme.beige)
-            .padding(.horizontal)
-            
-            Button {
-//                    postViewModel.createPost(with: captionText, image: imageSelected, userID: authViewModel.userID!, imageURL: authViewModel.imageURL, userName: authViewModel.displayName, email: authViewModel.email)
-            } label: {
-                Text("Post Picture".uppercased())
-                    .font(.title3)
-                    .fontWeight(.bold)
-                    .padding()
-                    .frame(height: 60)
-                    .frame(maxWidth: .infinity)
-                    .background(Color.MyTheme.purple)
-                    .cornerRadius(12)
-                    .padding(.horizontal)
-            } .tint(Color.MyTheme.yellow)
-                .alert(isPresented: $postViewModel.showAlert) {
+            VStack(spacing: 10) {
+                TextField(
+                    "",
+                    text: $captionText,
+                    prompt:
+                        Text("Add your caption here...")
+                        .foregroundColor(.gray)
+                )
+                .customTextField(background: Color.MyTheme.beige)
+                
+                Button {
+                    //                    postViewModel.createPost(with: captionText, image: imageSelected, userID: authViewModel.userID!, imageURL: authViewModel.imageURL, userName: authViewModel.displayName, email: authViewModel.email)
+                } label: {
+                    Text("Post Pictures".uppercased())
+                        .font(.title3)
+                        .fontWeight(.bold)
+                        .padding()
+                        .frame(height: 60)
+                        .frame(maxWidth: .infinity)
+                        .background(Color.MyTheme.purple)
+                        .cornerRadius(12)
+                } .tint(Color.MyTheme.yellow)
+            }.padding(.all,20)
+            .alert(isPresented: $postViewModel.showAlert) {
                     Alert(
                         title: Text(postViewModel.alertMessage),
                         message: nil,
@@ -96,16 +83,15 @@ struct PostImageView: View {
                     )
                 }
         }.overlay {
-            
             if postViewModel.isLoading {
                 CustomProgressView()
-//                ZStack {
-//                    Color.primary.opacity(0.2).edgesIgnoringSafeArea(.all)
-//                    ProgressView("Loading...")
-//                        .progressViewStyle(
-//                            CircularProgressViewStyle(tint: .cyan)
-//                        )
-//                }
+                //                ZStack {
+                //                    Color.primary.opacity(0.2).edgesIgnoringSafeArea(.all)
+                //                    ProgressView("Loading...")
+                //                        .progressViewStyle(
+                //                            CircularProgressViewStyle(tint: .cyan)
+                //                        )
+                //                }
                 
             }
             
